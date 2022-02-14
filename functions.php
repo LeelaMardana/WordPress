2570079e26b3b04316bb3de21686b4a0407a2815
Anton Antonov <91306218+MagicFirework@users.noreply.github.com>
2022-02-14 13:50:06 +0600

    added myhelp

--- functions.php
+++ functions.php
@@ -1,22 +1,24 @@
 <?php
 
-// Подключение Логотипа
+// Подключение Логотипа и Title
 
-if (!function_exists('warcraft_setup')) {
-  function warcraft_setup(){
+if (!function_exists('my_setup')) {
+  function my_setup()
+  {
     //  Добавляем пользовательский логотип
-    add_theme_support( 'custom-logo', [
+    add_theme_support('custom-logo', [
       'height'      => 55,
       'width'       => 130,
       'flex-width'  => false,
       'flex-height' => false,
       'header-text' => '',
       'unlink-homepage-logo' => false, // WP 5.5
-    ] );
+    ]);
     // Добавляем динамический <title>
     add_theme_support('title-tag');
   }
-  add_action( 'after_setup_theme', 'warcraft_setup' );
+  // хук событие
+  add_action('after_setup_theme', 'my_setup');
 }
 
 
@@ -24,9 +26,10 @@ if (!function_exists('warcraft_setup')) {
 Подключение стилей и скриптов
 */
 
-add_action('wp_enqueue_scripts', 'warcraft_scripts');
+// хук событие
+add_action('wp_enqueue_scripts', 'my_scripts');
 
-function warcraft_scripts()
+function my_scripts()
 {
   wp_enqueue_style('main', get_stylesheet_uri());
   wp_enqueue_style('bootstrap', get_template_directory_uri() . '/plugins/bootstrap/css/bootstrap.css', array('main'), null);
@@ -53,4 +56,74 @@ function warcraft_scripts()
   wp_enqueue_script('google-map', get_template_directory_uri() . '/plugins/google-map/gmap3.min.js', array('jquery'), '1.0.0', true);
   wp_enqueue_script('contact', get_template_directory_uri() . '/plugins/jquery/contact.js', array('jquery'), '1.0.0', true);
   wp_enqueue_script('custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0.0', true);
+}
+
+
+// Регистрируем сразу несколько областей меню
+function my_menus()
+{
+  // Собираем несколько зон (областей) меню
+  $locations = array(
+    'header'  => __('Header Menu', 'warcraft'),
+    'footer'   => __('Footer Menu', 'warcraft'),
+  );
+  // Регистрируем области меню, которые лежат в переменной $locations
+  register_nav_menus($locations);
+}
+
+add_action('init', 'my_menus');
+
+
+class bootstrap_4_walker_nav_menu extends Walker_Nav_menu
+{
+
+  function
+  start_lvl(&$output, $depth = 0, $args = NULL)
+  { // ul
+    $indent = str_repeat("\t", $depth); // indents the outputted HTML
+    $submenu = ($depth > 0) ? ' sub-menu' : '';
+    $output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+  }
+
+  function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
+  { // li a span
+
+    $indent = ($depth) ? str_repeat("\t", $depth) : '';
+
+    $li_attributes = '';
+    $class_names = $value = '';
+
+    $classes = empty($item->classes) ? array() : (array) $item->classes;
+
+    $classes[] = ($args->walker->has_children) ? 'dropdown' : '';
+    $classes[] = ($item->current || $item->current_item_anchestor) ? 'active' : '';
+    $classes[] = 'nav-item';
+    $classes[] = 'nav-item-' . $item->ID;
+    if ($depth && $args->walker->has_children) {
+      $classes[] = 'dropdown-menu';
+    }
+
+    $class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+    $class_names = ' class="' . esc_attr($class_names) . '"';
+
+    $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
+    $id = strlen($id) ? ' id="' . esc_attr($id) . '"' : '';
+
+    $output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
+
+    $attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+    $attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+    $attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+    $attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+
+    $attributes .= ($args->walker->has_children) ? ' class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
+
+    $item_output = $args->before;
+    $item_output .= ($depth > 0) ? '<a class="dropdown-item"' . $attributes . '>' : '<a' . $attributes . '>';
+    $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+    $item_output .= '</a>';
+    $item_output .= $args->after;
+
+    $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+  }
 }
--- header.php
+++ header.php
@@ -7,17 +7,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="seo & digital marketing" />
   <meta name="keywords" content="marketing,digital marketing,creative, agency, startup,promodise,onepage, clean, modern,seo,business, company" />
-  <?php wp_head()?>
+  <?php wp_head() ?>
 </head>
 
 <body data-spy="scroll" data-target=".fixed-top">
   <nav class="navbar navbar-expand-lg fixed-top trans-navigation">
     <div class="container">
       <?php
-        if( has_custom_logo() ){
-          // логотип есть выводим его
-          echo get_custom_logo();
-        }
+      if (has_custom_logo()) {
+        // логотип есть выводим его
+        echo get_custom_logo();
+      }
       ?>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon">
@@ -26,30 +26,18 @@
       </button>
 
       <div class="collapse navbar-collapse justify-content-end" id="mainNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/"> Главная </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarWelcome" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">О нас</a>
-            <div class="dropdown-menu" aria-labelledby="navbarWelcome">
-              <a class="dropdown-item" href="about.html"> О компании </a>
-              <a class="dropdown-item" href="about.html"> Об услугах </a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link smoth-scroll" href="service.html">Услуги</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link smoth-scroll" href="pricing.html">Цены</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link smoth-scroll" href="blog.html">Журнал</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link smoth-scroll" href="contact.html">Контакты</a>
-          </li>
-        </ul>
+        <?php
+        wp_nav_menu([
+          'theme_location'  => 'header',
+          'container'       => 'false',
+          'menu_class'      => 'navbar-nav',
+          'menu_id'         => false,
+          'echo'            => true,
+          'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+          'depth'           => 2,
+          'walker'          => new bootstrap_4_walker_nav_menu(),
+        ]);
+        ?>
       </div>
     </div>
   </nav>
